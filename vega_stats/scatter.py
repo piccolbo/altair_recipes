ba83ed13ba167bdcb1eabb9ddee03348b23294d1
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-07 21:54:29 -0700

    better defaults

--- vega_stats/scatter.py
+++ vega_stats/scatter.py
@@ -2,14 +2,14 @@ import altair as alt
 from vega_datasets import data
 
 
-def scatter(data, x="x", y="y", color=None, tooltip=None):
+def scatter(data, x="x", y="y", color=alt.Color(), tooltip=alt.Tooltip()):
     return alt.Chart(data).mark_point().encode(
         x=x, y=y, color=color, tooltip=tooltip)
 
 
 scatter(
     data.iris(),
-    x='petalWidth',
+    x="petalWidth",
     y='petalLength',
-    color='species',
-    tooltip='species')
+    color="sepalWidth",
+    tooltip="species")
