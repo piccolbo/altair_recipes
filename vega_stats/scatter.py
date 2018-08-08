8d6650b2d06118ef318d0fa4b23c8188579c2083
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-08 09:55:50 -0700

    new multiscatter plot

--- vega_stats/scatter.py
+++ vega_stats/scatter.py
@@ -7,9 +7,33 @@ def scatter(data, x="x", y="y", color=alt.Color(), tooltip=alt.Tooltip()):
         x=x, y=y, color=color, tooltip=tooltip)
 
 
+def multiscatter(data,
+                 variables=None,
+                 color=alt.Color(),
+                 tooltip=alt.Tooltip(),
+                 width=None,
+                 height=None):
+    variables = list(data.columns) if variables is None else list(variables)
+    ncol = data.shape[1]
+    width = 1600 // ncol if width is not None else int(width)
+    height = width is height is None else int(height) 
+    return alt.Chart(data).mark_point().encode(
+        alt.X(alt.repeat("column"), type='quantitative'),
+        alt.Y(alt.repeat("row"), type='quantitative'),
+        color=color).properties(
+            width=width, height=height).repeat(
+                row=variables, column=variables)
+
+
 scatter(
     data.iris(),
     x="petalWidth",
     y='petalLength',
     color="sepalWidth",
     tooltip="species")
+
+multiscatter(data.iris())
+
+multiscatter(data.iris(), variables=data.iris().columns[:-1], color="species")
+
+data.iris().columns[:-1]
