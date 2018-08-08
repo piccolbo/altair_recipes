43e99cef13399845aaca368173527b5f8dfc272f
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-08 15:51:48 -0700

    on omission and re-order

--- vega_stats/__init__.py
+++ vega_stats/__init__.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
-
 """Top-level package for vega-stats."""
+from .autocorrelation import autocorrelation
+from .boxplot import boxplot
+from .histogram import histogram, layered_histogram
+from .qqplot import qqplot
+from .scatter import scatter, multiscatter
+from .smoother import smoother
 
+# alphabetically ordered exports
+__all__ = [
+    'autocorrelation', 'boxplot', 'histogram', 'layered_histogram',
+    'multiscatter', 'qqplot', 'scatter', 'smoother'
+]
 __author__ = """Antonio Piccolboni"""
 __email__ = 'antonio@piccolboni.info'
 __version__ = '0.1.0'
