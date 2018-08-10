44b13bf112b60b11998d8c3529c682290c09ef04
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-10 16:04:44 -0700

    clean api, axis naming and data format change

--- altair_recipes/boxplot.py
+++ altair_recipes/boxplot.py
@@ -1,26 +1,27 @@
 """Boxplot implementation"""
 import altair as alt
-import pandas as pd
+from altair_recipes.common import default, gather
 
 
-def boxplot(data, x="x", y="y"):
-    if type(x) is not str:
-        cols = x
-        x = "variables" if x is None else x.name
-        data = pd.melt(
-            data,
-            id_vars=data.index.name,
-            value_vars=cols,
-            var_name=x,
-            value_name=y)
+def boxplot(data, columns, group_by=None, mark={}, encoding={}, properties={}):
+    assert type(columns) == str or len(columns) == 1 or group_by is None
+    # we are doing wide or long format but not both
+    if group_by is None:  #convert wide to long
+        x = default(data.columns.name, "variable")
+        y = "value"
+        data = gather(data, key=x, value=y, columns=columns)
+    else:
+        x = group_by
+        y = columns if type(columns) is str else columns[0]
+    #long form assumed from here
     chart = alt.Chart(data)
     chart_bar = chart.mark_bar(filled=False)
     chart_tick = chart.mark_tick()
     min_y = "min(" + y + ")"
     max_y = "max(" + y + ")"
     median_y = "median(" + y + ")"
-    q1_bar = chart_bar.encode(x=x, y="q1(" + y + "):Q", y2=median_y)
-    q3_bar = chart_bar.encode(x=x, y=median_y, y2="q3(" + y + "):Q")
+    q1_bar = chart_bar.encode(x=x, y="q1(" + y + ")", y2=median_y)
+    q3_bar = chart_bar.encode(x=x, y=median_y, y2="q3(" + y + ")")
 
     min_tick = chart_tick.encode(x=x, y=min_y)
     max_tick = chart_tick.encode(x=x, y=max_y)
