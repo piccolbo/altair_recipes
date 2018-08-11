2578b0ae28500640b81518b5a6111573c806b166
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-10 22:17:24 -0700

    save graphs for inspection

--- altair_recipes/common.py
+++ altair_recipes/common.py
@@ -3,6 +3,8 @@ def viz_reg_test(test_f):
         plot = test_f()
         if regtest is not None:
             regtest.write(plot.to_json())
+        plot.save(
+            test_f.__code__.co_filename + "_" + test_f.__qualname__ + ".html")
         return plot
 
     return fun
