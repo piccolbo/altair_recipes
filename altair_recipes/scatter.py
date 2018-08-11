cd00fe50b106efcc2482157c751afb7c01aef1d4
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-10 22:21:28 -0700

    semi-rewrite to simplify

--- altair_recipes/scatter.py
+++ altair_recipes/scatter.py
@@ -1,4 +1,5 @@
 import altair as alt
+from altair_recipes.common import default
 
 
 def scatter(data, x="x", y="y", mark={}, encoding={}, properties={}):
@@ -6,16 +7,10 @@ def scatter(data, x="x", y="y", mark={}, encoding={}, properties={}):
         x=x, y=y, **encoding).properties(**properties)
 
 
-    variables = list(data.columns) if variables is None else list(variables)
-    ncol = data.shape[1]
-    width = 1600 // ncol if width is None else int(width)
-    height = width if height is None else int(height)
-    return alt.Chart(data).mark_point().encode(
 def multiscatter(data, variables=None, mark={}, encoding={}, properties={}):
     variables = list(default(variables, data.columns))
     return alt.Chart(data).mark_point(**mark).encode(
         alt.X(alt.repeat("column"), type='quantitative'),
         alt.Y(alt.repeat("row"), type='quantitative'),
-        color=color).properties(
-            width=width, height=height).repeat(
-                row=variables, column=variables)
+        **encoding).properties(**properties).repeat(
+            row=variables, column=variables)
