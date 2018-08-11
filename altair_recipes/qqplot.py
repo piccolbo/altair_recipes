26bb2805e001ef13555162ce3c7abed84d175810
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-10 22:23:16 -0700

    unif signature with additional options

--- altair_recipes/qqplot.py
+++ altair_recipes/qqplot.py
@@ -3,8 +3,9 @@ import numpy as np
 from altair_recipes.common import to_dataframe
 
 
-def qqplot(data, x="x", y="y"):
+def qqplot(data, x="x", y="y", mark={}, encoding={}, properties={}):
     data = to_dataframe(data)
     n = data.shape[0]
     data = data.quantile(np.arange(0, 1, step=10 / n))
-    return alt.Chart(data).mark_point().encode(x=x, y=y)
+    return alt.Chart(data).mark_point(**mark).encode(
+        x=x, y=y, **encoding).properties(**properties)
