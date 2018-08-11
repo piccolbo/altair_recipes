4a68122427b27505f1f343074d1fdde7faa3e877
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-11 12:57:18 -0700

    use column instead of variable

--- altair_recipes/scatter.py
+++ altair_recipes/scatter.py
@@ -7,10 +7,10 @@ def scatter(data, x="x", y="y", mark={}, encoding={}, properties={}):
         x=x, y=y, **encoding).properties(**properties)
 
 
-def multiscatter(data, variables=None, mark={}, encoding={}, properties={}):
-    variables = list(default(variables, data.columns))
+def multiscatter(data, columns=None, mark={}, encoding={}, properties={}):
+    columns = list(default(columns, data.columns))
     return alt.Chart(data).mark_point(**mark).encode(
         alt.X(alt.repeat("column"), type='quantitative'),
         alt.Y(alt.repeat("row"), type='quantitative'),
         **encoding).properties(**properties).repeat(
-            row=variables, column=variables)
+            row=columns, column=columns)
