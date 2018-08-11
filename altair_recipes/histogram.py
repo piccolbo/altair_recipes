46c5fccaed2a88f169d49c5253ca4c917f267d73
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-10 22:22:02 -0700

    bring sigs in line

--- altair_recipes/histogram.py
+++ altair_recipes/histogram.py
@@ -9,14 +9,14 @@ def histogram(data, column, mark={}, encoding={}, properties={}):
 
 
 def layered_histogram(data,
-                      count_variables,
-                      color_variable=None,
-                      color_scale=alt.Scale(),
-                      x_label=None,
-                      y_label=None):
     if len(count_variables) == 1:
         x_label = count_variables[0] if x_label is None else str(x_label)
         assert color_variable is not None
+                      columns,
+                      group_by=None,
+                      mark={},
+                      encoding={},
+                      properties={}):
     data = to_dataframe(data)
     else:
         x_label = "Value" if x_label is None else str(x_label)
