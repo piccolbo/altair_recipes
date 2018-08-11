cdea88de03704862fa9df5aec41154f2e35f8c71
Antonio Piccolboni <antonio@piccolboni.info>
2018-08-10 22:13:24 -0700

    prepare data to access shape
    for non df sources

--- altair_recipes/autocorrelation.py
+++ altair_recipes/autocorrelation.py
@@ -1,9 +1,16 @@
 import altair as alt
 import numpy as np
 import pandas as pd
+from altair_recipes.common import to_dataframe
 
 
-def autocorrelation(data, variable, max_lag=None):
+def autocorrelation(data,
+                    column,
+                    max_lag=None,
+                    mark={},
+                    encoding={},
+                    properties={}):
+    data = to_dataframe(data)
     max_lag = data.shape[0] - 1 if max_lag is None else int(max_lag)
     lags = np.arange(0, max_lag + 1)
     _data = pd.DataFrame(
