1cd71c4c450cf89ec2c48639025165458ae0625e
Antonio Piccolboni <antonio@piccolboni.info>
2018-09-21 13:35:45 -0700

    undoing some bumpversion damage

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/piccolbo/altair_recipes',
-    version='version='0.3.2'',
+    version='0.3.2',
     zip_safe=False,
 )
