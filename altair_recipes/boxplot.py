88d7339a7bf157ecfe5160a4cce45f53d1d37956
Antonio Piccolboni <antonio@piccolboni.info>
2019-01-12 01:07:55 -0800

    faceted boxplots

--- altair_recipes/boxplot.py
+++ altair_recipes/boxplot.py
@@ -1,27 +1,40 @@
 """Boxplot implementation."""
 from .common import multivariate_preprocess
-from .signatures import multivariate_recipe
+from .signatures import multivariate_recipe, color
 import altair as alt
-from autosig import autosig
+from autosig import autosig, Signature
 
 
-@autosig(multivariate_recipe)
-def boxplot(data, columns=None, group_by=None, height=300, width=400):
+@autosig(multivariate_recipe + Signature(color=color(default=None, position=3)))
+def boxplot(data, columns=None, group_by=None, color=None, height=300, width=400):
     """Generate a boxplot."""
     data, key, value = multivariate_preprocess(data, columns, group_by)
     # long form assumed from here
-    chart = alt.Chart(data, height=height, width=width)
-    chart_bar = chart.mark_bar(filled=False)
+    chart = alt.Chart(
+        height=height,
+        width=width / ((len(data[key].unique()) if color is not None else 1)),
+    )
+    chart_bar = chart.mark_bar(stroke="black", fill="#4682b4")
     chart_tick = chart.mark_tick()
+    encode_args = dict(x=key) if color is None else dict(x=alt.X(color, axis=None))
     min_value = "min(" + value + ")"
     max_value = "max(" + value + ")"
     median_value = "median(" + value + ")"
-    q1_bar = chart_bar.encode(x=key, y="q1(" + value + ")", y2=median_value)
-    q3_bar = chart_bar.encode(x=key, y=median_value, y2="q3(" + value + ")")
+    min_tick = chart_tick.encode(y=min_value, **encode_args)
+    max_tick = chart_tick.encode(y=max_value, **encode_args)
+    q1 = "q1(" + value + ")"
+    q3 = "q3(" + value + ")"
+    if color is not None:
+        encode_args["color"] = color
+    q1_bar = chart_bar.encode(y=q1, y2=median_value, **encode_args)
+    q3_bar = chart_bar.encode(y=median_value, y2=q3, **encode_args)
 
-    min_tick = chart_tick.encode(x=key, y=min_value)
-    max_tick = chart_tick.encode(x=key, y=max_value)
     rule = chart.mark_rule().encode(
-        x=key, y=alt.Y(min_value, axis=alt.Axis(title=value)), y2=max_value
+        y=alt.Y(min_value, axis=alt.Axis(title=value)), y2=max_value, **encode_args
     )
-    return q1_bar + q3_bar + min_tick + max_tick + rule
+    bplot = rule + min_tick + max_tick + q1_bar + q3_bar
+    return (
+        bplot.properties(data=data)
+        if color is None
+        else bplot.facet(data=data, column=key)
+    )  # + q3_bar + min_tick + max_tick
