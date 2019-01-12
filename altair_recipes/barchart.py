73b1d45edff1d942401142902cc3889ebdc3cae1
Antonio Piccolboni <antonio@piccolboni.info>
2019-01-11 23:12:07 -0800

    barchart feature

---
+++ altair_recipes/barchart.py
@@ -0,0 +1,18 @@
+"""Generate barcharts."""
+from .signatures import bivariate_recipe, color
+from autosig import autosig, Signature
+import altair as alt
+
+
+@autosig(bivariate_recipe + Signature(color=color(default=None, position=3)))
+def barchart(data, x=0, y=1, color=None, height=300, width=400):
+    """Generate a barchart."""
+    chart = alt.Chart(
+        data,
+        height=height,
+        width=width / ((len(data[x].unique()) if color is not None else 1)),
+    ).mark_bar()
+    if color is None:
+        return chart.encode(x=x, y=y)
+    else:
+        return chart.encode(x=alt.X(color, axis=None), y=y, color=color, column=x)
