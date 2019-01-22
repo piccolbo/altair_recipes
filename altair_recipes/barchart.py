3bdd755e43c2622235b2646e6546da6ec922cfff
Antonio Piccolboni <antonio@piccolboni.info>
2019-01-22 11:14:51 -0800

    doc reflects changes

--- altair_recipes/barchart.py
+++ altair_recipes/barchart.py
@@ -6,7 +6,7 @@ from autosig import autosig, Signature
 
 @autosig(bivariate_recipe + Signature(color=color(default=None, position=3)))
 def barchart(data, x=0, y=1, color=None, height=600, width=800):
-    """Generate a barchart, optionally faceted."""
+    """Generate a barchart."""
     enc_args = dict(x=x, y=y)
     if color is not None:
         enc_args["color"] = color
