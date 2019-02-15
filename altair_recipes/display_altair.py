c6cef59a34b4ea9d7849ee17e471078a2a521a4b
Antonio Piccolboni <antonio@piccolboni.info>
2019-02-14 16:25:31 -0800

    added mode for pweave md production

--- altair_recipes/display_altair.py
+++ altair_recipes/display_altair.py
@@ -1,28 +1,30 @@
 """Allow including vega graphics in pweave docs."""
 from IPython.core.display import display_html
 import hashlib
+from enum import Enum
 
-display_html(
-    """
+
+Output = Enum("Output", "pweave_md pweave_html")
+
+has_preamble = False
+
+
+def to_html(plot):
+    """Generate html from plot for inclusion in md or html doc."""
+    global has_preamble
+    json = plot.to_json()
+    id = "A" + hashlib.sha256(json.encode()).hexdigest()
+    html = (
+        (
+            ""
+            if has_preamble
+            else """
 <script src="https://cdn.jsdelivr.net/npm/vega@3"></script>
 <script src="https://cdn.jsdelivr.net/npm/vega-lite@2"></script>
 <script src="https://cdn.jsdelivr.net/npm/vega-embed@3"></script>
-""",
-    raw=True,
-)
-
-
-def show(plot):
-    """
-    Include an Altair (vega) figure in Pweave document.
-
-    Generates html output.
-    """
-    json = plot.to_json()
-    id = "A" + hashlib.sha256(json.encode()).hexdigest()
-    display_html(
-        """
- <div id="{id}"></div>
+ """
+        )
+        + """<div id="{id}"></div>
   <script type="text/javascript">
     var spec = {json};
     var opt = {{"renderer": "canvas", "actions": false}};
@@ -30,9 +32,23 @@ def show(plot):
   </script>
   """.format(
             id=id, json=json
-        ),
-        raw=True,
+        )
     )
+    has_preamble = True
+    return html
+
+
+def show(plot, output=Output.pweave_md):
+    """
+    Include an Altair (vega) figure in Pweave document.
+
+    Generates html output.
+    """
+    html = to_html(plot)
+    if output == Output.pweave_md:
+        display_html(html, raw=True)
+    else:
+        print(html)
 
 
 def show_test(f):
