ffa822fe3ba8b187e0adacba5f6827862485ad8f
Antonio Piccolboni <antonio@piccolboni.info>
2019-08-15 10:40:06 -0700

    formatting

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
@@ -21,33 +21,34 @@ requirements = [
     "toolz>=0.10.0",
 ]
 
-setup_requirements = ['pytest-runner']
 
-test_requirements = ['pytest']
+setup_requirements = ["pytest-runner"]
+
+test_requirements = ["pytest"]
 
 setup(
     author="Antonio Piccolboni",
-    author_email='antonio@piccolboni.info',
+    author_email="antonio@piccolboni.info",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
     description="A collection of ready-made statistical graphics for vega",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='altair_recipes',
-    name='altair_recipes',
-    packages=find_packages(include=['altair_recipes']),
+    keywords="altair_recipes",
+    name="altair_recipes",
+    packages=find_packages(include=["altair_recipes"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/piccolbo/altair_recipes',
-    version='0.6.0',
+    url="https://github.com/piccolbo/altair_recipes",
+    version="0.6.0",
     zip_safe=False,
 )
