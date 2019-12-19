3e089641e4d0208ccede1dfe70e9547f889ad816
Antonio Piccolboni <antonio@piccolboni.info>
2019-12-19 15:05:52 -0800

    fixes #15

--- altair_recipes/boxplot.py
+++ altair_recipes/boxplot.py
@@ -11,23 +11,21 @@ def boxplot(data=None, columns=None, group_by=None, color=False, height=600, wid
     data, key, value = multivariate_preprocess(data, columns, group_by)
     # long form assumed from here
     chart = alt.Chart(height=height, width=width)
-    chart_bar = chart.mark_bar(stroke="black", fill="#4682b4")  # default blu
-    chart_tick = chart.mark_tick()
-    encode_args = dict(x=key + ":N")
+    chart_bar = chart.mark_bar(stroke="black", fill="#4682b4")
+
+    chart_tick = chart.mark_tick(color="black")
+    x = key + ":N"
     stats = {
         stat: stat + "(" + value + ")" for stat in ["min", "max", "median", "q1", "q3"]
     }
-    ticks = {
-        tick: chart_tick.encode(y=stats[tick], **encode_args) for tick in ["min", "max"]
-    }
+    ticks = {tick: chart_tick.encode(x=x, y=stats[tick]) for tick in ["min", "max"]}
+    encode_args = dict(x=x)
     if color:
-        encode_args["color"] = key + ":N"
+        encode_args["fill"] = key + ":N"
     q1_bar = chart_bar.encode(y=stats["q1"], y2=stats["median"], **encode_args)
     q3_bar = chart_bar.encode(y=stats["median"], y2=stats["q3"], **encode_args)
 
     rule = chart.mark_rule().encode(
-        y=alt.Y(stats["min"], axis=alt.Axis(title=value)),
-        y2=stats["max"],
-        **encode_args
+        x=x, y=alt.Y(stats["min"], axis=alt.Axis(title=value)), y2=stats["max"]
     )
     return (rule + ticks["min"] + ticks["max"] + q1_bar + q3_bar).properties(data=data)
